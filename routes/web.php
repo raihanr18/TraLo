30ae414387ec874f46c06c8536565f1e36ac6c5c
Fernaldy <155967997+Aldy91@users.noreply.github.com>
2024-06-14 12:39:24 +0700

    Menambahkan route kedalam file web.php

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,31 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LoginController;
 
 /*
 |--------------------------------------------------------------------------
 | Web Routes
 |--------------------------------------------------------------------------
 |
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
+| Here is where you can register web routes for your application.
+| These routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
 |
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
+
+Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
+Route::post('/login', [LoginController::class, 'login']);
+
+Route::get('/signup', [LoginController::class, 'showsignup'])->name('signup');
+Route::post('/signup', [LoginController::class, 'signup']);
+
+Route::get('/forgotpassword', [LoginController::class, 'showsforgotpassword'])->name('forgotpassword');
+Route::post('/forgotpassword', [LoginController::class, 'forgotpassword']);
+
+Route::get('/resetPassword', [LoginController::class, 'showsresetPassword'])->name('resetPassword');
+Route::post('/resetPassword', [LoginController::class, 'showsresetPassword'])->name('resetPassword');
