33129a7f7fa4e71dd034299242687baee7799c87
Fernaldy <155967997+Aldy91@users.noreply.github.com>
2024-06-21 01:54:05 +0700

    Mengubah route di file web.php

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LoginController;
+use App\Http\Controllers\ForgotController;
+use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\ResetController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\MyticketController;
+use App\Http\Controllers\MyorderController;
+use App\Http\Controllers\MyaccountController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -21,11 +29,23 @@ Route::get('/', function () {
 Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
 Route::post('/login', [LoginController::class, 'login']);
 
-Route::get('/signup', [LoginController::class, 'showsignup'])->name('signup');
-Route::post('/signup', [LoginController::class, 'signup']);
+Route::get('/signup', [RegisterController::class, 'showsignup'])->name('signup');
+Route::post('/signup', [RegisterController::class, 'signup']);
 
-Route::get('/forgotpassword', [LoginController::class, 'showsforgotpassword'])->name('forgotpassword');
-Route::post('/forgotpassword', [LoginController::class, 'forgotpassword']);
+Route::get('/forgotpassword', [ForgotController::class, 'showsforgotpassword'])->name('forgotpassword');
+Route::post('/forgotpassword', [ForgotController::class, 'forgotpassword']);
 
-Route::get('/resetPassword', [LoginController::class, 'showsresetPassword'])->name('resetPassword');
-Route::post('/resetPassword', [LoginController::class, 'showsresetPassword'])->name('resetPassword');
+Route::get('/resetPassword', [ResetController::class, 'showsresetPassword'])->name('resetPassword');
+Route::post('/resetPassword', [ResetController::class, 'showsresetPassword'])->name('resetPassword');
+
+Route::get('/dashboard', [DashboardController::class, 'showdashboard'])->name('dashboard');
+Route::post('/dashboard', [DashboardController::class, 'showdashboard'])->name('dashboard');
+
+Route::get('/myticket', [MyticketController::class, 'showmyticket'])->name('myticket');
+Route::post('/myticket', [MyticketController::class, 'showmyticket'])->name('myticket');
+
+Route::get('/myorder', [MyorderController::class, 'showmyorder'])->name('myorder');
+Route::post('/myorder', [MyorderController::class, 'showmyorder'])->name('myorder');
+
+Route::get('/myaccount', [MyaccountController::class, 'showmyaccount'])->name('myaccount');
+Route::post('/myaccount', [MyaccountController::class, 'showmyaccount'])->name('myaccount');
